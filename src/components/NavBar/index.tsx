582f3826047a6362081248552f3a6dbb9dbb1176
J3duardo <jegq.dev.projects@gmail.com>
2022-12-28 09:52:26 -0400

    Botones dinámicos y spinner del navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,5 +1,11 @@
-import {NavLink} from "react-router-dom";
+import {useState} from "react";
+import {NavLink, useNavigate} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
 import {AppBar, Toolbar, Button, Box} from "@mui/material";
+import Spinner from "../Spinner";
+import {logoutUser} from "../../redux/features/authSlice";
+import {AuthState} from "../../redux/store";
+import {auth} from "../../firebase";
 import "./navBar.css";
 
 const MENU_ITEMS__LEFT = [
@@ -8,12 +14,31 @@ const MENU_ITEMS__LEFT = [
   {id: 3, title: "About", to: "/about"},
 ];
 
-const MENU_ITEMS__RIGHT = [
+const MENU_ITEMS_NOAUTH = [
   {id: 4, title: "Login", to: "/login"},
   {id: 5, title: "Signup", to: "/signup"}
 ];
 
 const NavBar = () => {
+  const {isAuth, user, loading} = useSelector((state: AuthState) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [loggingOut, setLoggingOut] = useState(false);
+
+  const logoutHandler = async () => {
+    try {
+      setLoggingOut(true);
+      await auth.signOut();
+      dispatch(logoutUser());
+      navigate("/login", {replace: true});      
+    } catch (error: any) {
+      console.log(`Error logging user out`, error.message);
+    } finally {
+      setLoggingOut(false);
+    }
+  };
+
   return (
     <AppBar component="nav" position="fixed">
       <Toolbar className="navbar__toolbar inner-wrapper">
@@ -39,32 +64,74 @@ const NavBar = () => {
             )
           })}
         </Box>
-        <Box className="navbar__items">
-          {MENU_ITEMS__RIGHT.map((el) => {
-            return (
-              <NavLink
-                key={el.id}
-                to={el.to}
-                className="navbar__item"
-              >
-                {({isActive}) => {
-                  return (
-                    <Button
-                      key={el.id}
-                      className={isActive ? "navbar__item__btn navbar__item__btn--active" : "navbar__item__btn"}
-                      disableRipple
-                    >
-                      {el.title}
-                    </Button>
-                  )
-                }}
-              </NavLink>
-            )
-          })}
-        </Box>
+
+        {loading &&
+          <Box className="navbar__spinner" position="relative">
+            <Spinner
+              containerHeight="auto"
+              spinnerWidth="1.2rem"
+              spinnerHeight="1.2rem"
+              spinnerColor="white"
+            />
+          </Box>
+        }
+
+        {!isAuth && !loading &&
+          <Box className="navbar__items">
+            {MENU_ITEMS_NOAUTH.map((el) => {
+              return (
+                <NavLink
+                  key={el.id}
+                  to={el.to}
+                  className="navbar__item"
+                >
+                  {({isActive}) => {
+                    return (
+                      <Button
+                        key={el.id}
+                        className={isActive ? "navbar__item__btn navbar__item__btn--active" : "navbar__item__btn"}
+                        disableRipple
+                      >
+                        {el.title}
+                      </Button>
+                    )
+                  }}
+                </NavLink>
+              )
+            })}
+          </Box>
+        }
+
+        {isAuth && !loading &&
+          <Box className="navbar__items">
+            <NavLink
+              to="/profile"
+              className="navbar__item"
+            >
+              {({isActive}) => {
+                return (
+                  <Button
+                    className={isActive ? "navbar__item__btn navbar__item__btn--active" : "navbar__item__btn"}
+                    disableRipple
+                  >
+                    {user?.displayName}
+                  </Button>
+                )
+              }}
+            </NavLink>
+            <Button
+              className="navbar__item__btn"
+              disabled={loggingOut}
+              onClick={logoutHandler}
+              disableRipple
+            >
+              Logout
+            </Button>
+          </Box>
+        }
       </Toolbar>
     </AppBar>
   )
 }
 
-export default NavBar
+export default NavBar;
