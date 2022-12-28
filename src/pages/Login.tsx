91e839aff719a2c5b470cb5580345f143a037ba0
J3duardo <jegq.dev.projects@gmail.com>
2022-12-27 22:34:27 -0400

    Funcionalidad para inicio de sesión de usuarios

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,72 @@
 import {useState} from "react";
-import {Box, Button, Typography} from "@mui/material";
+import {Box, Button, Typography, Alert} from "@mui/material";
 import {useForm, FormProvider} from "react-hook-form";
+import {useNavigate} from "react-router-dom";
 import {AiOutlineLogin} from "react-icons/ai";
+import {AuthError, signInWithEmailAndPassword} from "firebase/auth";
 import {EmailField, PasswordField} from "../components/AuthFormsElements";
+import {generateFirebaseErrorMsg} from "../utils/firebaseErrorMessages";
+import {auth} from "../firebase";
 import "../styles/authForms.css";
 
 export interface LoginFormFields {
   email: string;
   password: string;
-}
+};
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const [loading, setLoading] = useState(false);
+  const [backendError, setBackendError] = useState<null | string>(null);
+
   const methods = useForm<LoginFormFields>({mode: "onSubmit"});
 
   const onSubmitHandler = async (values: LoginFormFields) => {
-    console.log({values});
+    const {email, password} = values;
+
+    setLoading(true);
+    setBackendError(null);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/", {replace: true});
+
+    } catch (error: unknown) {
+      const authErr = error as AuthError;
+      const errMessage = generateFirebaseErrorMsg(authErr.code);
+
+      if (errMessage.includes("email") && errMessage.includes("password")) {
+        methods.setError("email", {
+          type: "Firebase email error",
+          message: errMessage
+        });
+
+        return methods.setError("password", {
+          type: "Firebase password error",
+          message: errMessage
+        });
+      };
+
+      if (errMessage.toLowerCase().includes("email")) {
+        return methods.setError("email", {
+          type: "Firebase email error",
+          message: errMessage
+        });
+      };
+
+      if (errMessage.toLowerCase().includes("password")) {
+        return methods.setError("password", {
+          type: "Firebase password error",
+          message: errMessage
+        });
+      };
+      
+      setBackendError(errMessage);
+      
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -23,14 +74,25 @@ const Login = () => {
       <Typography variant="h2" marginBottom="1.5rem">
         Login
       </Typography>
+
+      {backendError &&
+        <Alert
+          style={{marginBottom: "1.5rem"}}
+          severity="error"
+          onClose={() => setBackendError(null)}
+        >
+          {backendError}
+        </Alert>
+      }
+
       <FormProvider {...methods}>
         <form
           className="authForm__form"
           onSubmit={methods.handleSubmit(onSubmitHandler)}
           noValidate
         >
-          <EmailField />
-          <PasswordField />
+          <EmailField disabled={loading} />
+          <PasswordField disabled={loading} />
           <Button
             className="authForm__button"
             variant="outlined"
