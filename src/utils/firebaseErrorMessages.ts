2f5c033721567fbfa702e3c3df550e089290545c
J3duardo <jegq.dev.projects@gmail.com>
2022-12-30 10:35:03 -0400

    Generar mensajes de error de firebase storage a partir del código del error

--- src/utils/firebaseErrorMessages.ts
+++ src/utils/firebaseErrorMessages.ts
@@ -1,4 +1,8 @@
-export const generateFirebaseErrorMsg = (errorCode: string) => {
+/**
+ * Generar mensajes de error de autenticación de firebase
+ * a partir del código del error.
+ */
+export const generateFirebaseAuthErrorMsg = (errorCode: string) => {
   console.log({errorCode});
   let errMessage: string;
 
@@ -26,6 +30,45 @@ export const generateFirebaseErrorMsg = (errorCode: string) => {
       break;
     default:
       errMessage = "There was an unknown error. Try again later."
+  };
+
+  return errMessage;
+};
+
+
+/**
+ * Generar mensajes de error de storage de firebase
+ * a partir del código del error.
+ */
+export const generateFirebaseStorageErrorMsg = (errorCode: string) => {
+  console.log({errorCode});
+
+  let errMessage: string;
+
+  switch(errorCode) {
+    case "storage/object-not-found":
+      errMessage = "Image not found or deleted."
+      break;
+    case "storage/bucket-not-found":
+      errMessage = "Storage bucket not found or not properly configured."
+      break;
+    case "storage/unauthenticated":
+      errMessage = "Unauthorized. Please, log in to your account and try again."
+      break;
+    case "storage/unauthorized":
+      errMessage = "You are not authorized to perform the desired action."
+      break;
+    case "storage/retry-limit-exceeded":
+      errMessage = "Too many attempts. Try again later."
+      break;
+    case "storage/canceled":
+      errMessage = "The user cancelled the operation."
+      break;
+    case "storage/server-file-wrong-size":
+      errMessage = "The file does not match the size of the file received by the server. Try again."
+      break;
+    default:
+      errMessage = "An unknown error occurred. Try again later."
   };
 
   return errMessage;
