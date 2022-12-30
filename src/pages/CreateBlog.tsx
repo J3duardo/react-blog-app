6b4cf7a350baf99f5ed68a87111ee210c7ba994b
J3duardo <jegq.dev.projects@gmail.com>
2022-12-30 11:43:14 -0400

    Funcionalidad para subir la imagen del blog y crear el blog

--- src/pages/CreateBlog.tsx
+++ src/pages/CreateBlog.tsx
@@ -1,31 +1,43 @@
 import { useState, useEffect } from "react";
-import { Box, Typography, Button, Divider } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { Box, Typography, Button, Divider, Alert } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useForm, FormProvider } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { AiOutlinePlus } from "react-icons/ai";
+import { getDownloadURL, ref, StorageError, uploadBytesResumable } from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { BlogTitleField, CategorySelector, DescriptionField, FileInput } from "../components/CreateBlogFormElements";
-import { LayoutState } from "../redux/store";
+import LinearProgressBar from "../components/LinearProgressBar";
+import { isEmptyField } from "../components/CreateBlogFormElements/isEmptyField";
+import { AuthState, LayoutState } from "../redux/store";
+import { generateFirebaseStorageErrorMsg } from "../utils/firebaseErrorMessages";
+import { db, storage } from "../firebase";
 import "../styles/createBlogPage.css";
+import { setOpen } from "../redux/features/snackbarSlice";
 
-interface BlogFormFields {
+export interface BlogFormFields {
   title: string;
-  tags: string[];
   categories: string[];
   description: string;
-  image?: Blob;
+  image: File;
 };
 
 const CreateBlog = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const {user} = useSelector((state: AuthState) => state.auth);
   const {pagePadding} = useSelector((state: LayoutState) => state.layout);
   
-  const [image, setImage] = useState<Blob | null>(null);
+  const [image, setImage] = useState<File | null>(null);
+  const [fileName, setFileName] = useState<string>("");
   const [filePreview, setFilePreview] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState<number | null>(null);
   const [backendError, setBackendError] = useState<null | string>(null);
 
   const methods = useForm<BlogFormFields>({mode: "onSubmit"});
+
 
   /*--------------------------------------------*/
   // Generar el preview de la imagen seleccionda
@@ -34,54 +46,88 @@ const CreateBlog = () => {
     if(image) {
       methods.setValue("image", image);
       const objectUrl = URL.createObjectURL(image);
+      const imgName = `${user?.uid}-${Date.now()}-${image.name}`;
+      setFileName(imgName);
       setFilePreview(objectUrl);
     } else {
       setFilePreview(null);
     };
-
   }, [image]);
 
+
+  /*---------------------------------*/
+  // Subir la imagen y crear el blog
+  // al finalizar la subida
+  /*---------------------------------*/
   const onSubmitHandler = (values: BlogFormFields) => {
-    const title = methods.getValues("title");
-    const categories = methods.getValues("categories");
-    const description = methods.getValues("description");
-    let isInvalid = false;
+    // Verificar si hay campos vacíos
+    let isEmpty =
+      isEmptyField(methods, "title") ||
+      isEmptyField(methods, "categories") ||
+      isEmptyField(methods, "description");
 
-    // Retornar error de validación
-    // si el campo de título sólo contiene espacios
-    if(title.trim().length === 0) {
-      methods.setError("title", {
-        type: "required",
-        message: "The title is required"
-      });
-      
-      isInvalid = true;
-    };
-    
-    // Retornar error de validación
-    // si el array de categorías está vacío
-    if(categories.length === 0) {
-      methods.setError("categories", {
-        type: "required"
-      });
-
-      isInvalid = true;
-    };
-    
-    // Retornar error de validación
-    // si el campo de descripción sólo contiene espacios
-    if(description.trim().length === 0) {
-      methods.setError("description", {
-        type: "required",
-        message: "The description is required"
-      });
-
-      isInvalid = true;
+    // Si hay campos vacíos, retornar sin ejecutar el proceso
+    if(isEmpty) {
+      return null;
     };
 
-    if(isInvalid) return methods;
-    
-    console.log({values});
+    setLoading(true);
+    setBackendError(null);
+
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image!);
+
+    // Monitorear el progreso de la subida,
+    // manejar errores generados al subir el archivo
+    // y especificar la url del archivo al terminar la subida.
+    uploadTask.on("state_changed",
+      (snapshot) => {
+        setUploadProgress(() => {
+          const progress = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+          return Number(progress.toFixed(2));
+        });
+
+      },
+      (error: StorageError) => {
+        const code = error.code;
+        const msg = generateFirebaseStorageErrorMsg(code);
+        setBackendError(msg);
+        setUploadProgress(null);
+        setLoading(false);
+
+      },
+      async () => {
+        try {
+          const url = await getDownloadURL(uploadTask.snapshot.ref);
+
+          const blogData = {
+            author: user,
+            title: values.title,
+            categories: values.categories,
+            description: values.description,
+            imageUrl: url,
+            createdAt: serverTimestamp()
+          };
+
+          const res = await addDoc(collection(db, "blogs"), blogData);
+
+          dispatch(setOpen({open: true, message: "Blog created successfully!"}));
+          
+          // Navegar a la página del blog
+          navigate(`/blog/${res.id}`);
+          
+        } catch (error: unknown) {
+          const storageError = error as StorageError;
+          const code = storageError.code;
+          const errMsg = generateFirebaseStorageErrorMsg(code);
+          setBackendError(errMsg);
+
+        } finally {
+          setLoading(false);
+          setUploadProgress(null);
+        };
+      }
+    );
   };
 
 
@@ -95,17 +141,34 @@ const CreateBlog = () => {
       <Typography variant="h3" marginBottom="1.5rem">
         Create Blog
       </Typography>
+
+      {backendError &&
+        <Alert
+          style={{marginBottom: "1.5rem"}}
+          severity="error"
+          onClose={() => setBackendError(null)}
+        >
+          {backendError}
+        </Alert>
+      }
+
       <FormProvider {...methods}>
         <form
           className="create-blog__form"
           onSubmit={methods.handleSubmit(onSubmitHandler)}
         >
           <BlogTitleField disabled={loading} />
-          <CategorySelector />
+          <CategorySelector disabled={loading} />
           <DescriptionField disabled={loading} />
-          <FileInput imagePreview={filePreview} setImage={setImage} />
+          <FileInput
+            imagePreview={filePreview}
+            setImage={setImage}
+            disabled={loading}
+          />
 
           <Divider style={{width: "100%"}} />
+
+          <LinearProgressBar uploadProgress={uploadProgress} />
           
           <Button
             className="create-blog__button"
@@ -114,7 +177,9 @@ const CreateBlog = () => {
             endIcon={<AiOutlinePlus />}
             disabled={loading}
           >
-            Create Blog
+            {!loading && !uploadProgress && "Create blog"}
+            {loading && uploadProgress! < 100 && "Uploading blog..."}
+            {loading && uploadProgress === 100 && "Finishing upload..."}
           </Button>
         </form>
       </FormProvider>
