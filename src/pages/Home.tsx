9875b4d01b08de87adc43ef2794b39e244f7e42b
J3duardo <jegq.dev.projects@gmail.com>
2023-01-01 15:00:02 -0400

    Layout del homepage, cargar y mostrar la lista de blogs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
-import {Typography} from "@mui/material";
+import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+import BlogSection from "../components/HomePage/BlogSection";
+import MostPopular from "../components/HomePage/MostPopularSection";
+import { AuthState, LayoutState } from "../redux/store";
+import useFetchBlogs from "../hooks/useFetchBlogs";
+import "../styles/homePage.css";
 
 const Home = () => {
+  const {user} = useSelector((state: AuthState) => state.auth);
+  const {pagePadding: {top, bottom}} = useSelector((state: LayoutState) => state.layout);
+
+  const {loading, blogs} = useFetchBlogs();
+
   return (
-    <Typography>
-      Home page
-    </Typography>
+    <Box
+      style={{paddingTop: top, paddingBottom: bottom}}
+      className="home-page inner-wrapper"
+      component="main"
+    >
+      <Box className="home-page__main-section" component="section">
+        <BlogSection currentUser={user} blogs={blogs} loading={loading} />
+        <MostPopular />
+      </Box>
+    </Box>
   )
-}
+};
 
 export default Home;
