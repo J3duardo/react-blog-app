a2e69712e2de920bfea8846e9665d4b31aee4802
J3duardo <jegq.dev.projects@gmail.com>
2023-01-01 14:49:19 -0400

    Columna derecha del layout del home page (sin contenido)

---
+++ src/components/HomePage/MostPopularSection/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+const MostPopular = () => {
+  return (
+    <Box className="home-page__most-popular-section" component="article">
+      <Typography variant="h5">
+        Most popular blogs
+      </Typography>
+    </Box>
+  )
+}
+
+export default MostPopular
