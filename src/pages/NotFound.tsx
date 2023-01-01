08253776a87ecf8ab4b197ae09d80ab60a815bcd
J3duardo <jegq.dev.projects@gmail.com>
2023-01-01 17:42:36 -0400

    Página 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,38 @@
-const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+import { Box, Button, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { AiOutlineWarning } from "react-icons/ai";
+import {MdOutlineArrowBack} from "react-icons/md";
+import { LayoutState } from "../redux/store";
+import "../styles/notFoundPage.css";
 
-export default NotFound
+const NotFound = () => {
+  const navigate = useNavigate();
+  const {navbarHeight} = useSelector((state: LayoutState) => state.layout);
+
+  return (
+    <Box
+      className="not-found"
+      style={{padding: `${navbarHeight}px 0`}}
+    >
+      <Typography
+        className="not-found__title"
+        variant="body1"
+      >
+        Page not found!
+      </Typography>
+
+      <AiOutlineWarning className="not-found__icon" color="var(--mui-error)" />
+
+      <Button
+        color="error"
+        size="large"
+        startIcon={<MdOutlineArrowBack />}
+        onClick={() => navigate("/", {replace: true})}>
+        Go back
+      </Button>
+    </Box>
+  )
+};
+
+export default NotFound;
