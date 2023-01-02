e3fd146a4aa2a946545c0a61ff0f048d2655db8b
J3duardo <jegq.dev.projects@gmail.com>
2023-01-02 17:23:28 -0400

    Utilidad para formatear la fecha del documento de Firebase

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,11 @@
+import dayjs from "dayjs";
+import { Timestamp } from "firebase/firestore";
+
+/**
+ * Formatear la fecha generada por Firebase.
+ * @param date Representación `Timestamp` de la fecha generada por Firebase.
+ * @param format String opcional para formatear la fecha.
+ */
+export const dateFormatter = (date: Timestamp, format = "MM/DD/YYYY - hh:mm a") => {
+  return dayjs(date.toDate()).format(format);
+};
