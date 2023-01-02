d94921c56b6d88124c64863a55c52093e4b41011
J3duardo <jegq.dev.projects@gmail.com>
2023-01-02 19:14:56 -0400

    Cambios de layout del skeleton del blog card

--- src/components/BlogCard/BlogCardSkeleton.tsx
+++ src/components/BlogCard/BlogCardSkeleton.tsx
@@ -5,17 +5,27 @@ const BlogCardSkeleton = () => {
   return (
     <Box className="blog-card">
       <Box className="blog-card__img-wrapper">
-        <Skeleton width="100%" height={250}  />
+        <Skeleton
+          style={{
+            width: "100%",
+            height: "250px",
+            transform: "translateY(0)"
+          }}
+        />
       </Box>
       <Box className="blog-card__content">
         <Skeleton variant="text" style={{fontSize: "var(--heading-3)"}} />
-        <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
-        <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
+        <Box style={{display: "flex", alignItems: "center", gap: "10px"}}>
+          <Skeleton variant="circular" width={35} height={35} />
+          <Skeleton width={"90%"} style={{fontSize: "var(--paragraph)"}} />
+        </Box>
         <Box className="blog-card__categories">
           <Skeleton width={"30%"} height={45} />
           <Skeleton width={"30%"} height={45} />
           <Skeleton width={"30%"} height={45} />
         </Box>
+        <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
+        <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
         <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
         <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
         <Skeleton variant="text" style={{fontSize: "var(--paragraph)"}} />
