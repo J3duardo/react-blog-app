d3cc4d7641dc575ab409a74a746762c5faadb23f
J3duardo <jegq.dev.projects@gmail.com>
2023-01-03 10:49:38 -0400

    Funcionalidad para abrir la imagen del blog en el modal genérico de imágenes

--- src/pages/BlogDetails.tsx
+++ src/pages/BlogDetails.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Divider, IconButton, Tooltip, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { doc, getDoc } from "firebase/firestore";
+import { AiOutlineCamera } from "react-icons/ai";
 import { LayoutState } from "../redux/store";
 import { Blog } from "../components/HomePage/BlogSection";
 import { blogsCollection } from "../firebase";
 import BlogMetadata from "../components/BlogMetadata";
 import Spinner from "../components/Spinner";
 import "../styles/blogDetailsPage.css";
+import ImageModal from "../components/ImageModal";
 
 const BlogDetails = () => {
   const {blogId} = useParams();
@@ -17,6 +19,8 @@ const BlogDetails = () => {
   const [blogDetails, setBlogDetails] = useState<Blog | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
+  const [openImageModal, setOpenImageModal] = useState(false);
 
   // Cargar la data del blog
   useEffect(() => {
@@ -49,7 +53,7 @@ const BlogDetails = () => {
     )
   };
 
-  if(!blogDetails && error) {
+  if(!blogDetails && !loading) {
     return null
   };
 
@@ -59,6 +63,12 @@ const BlogDetails = () => {
       className="blog-detail"
       component="section"
     >
+      <ImageModal
+        image={blogDetails!.imageUrl}
+        open={openImageModal}
+        setOpen={setOpenImageModal}
+      />
+
       <Box
         className="blog-detail__img-wrapper inner-wrapper-xl"
         style={{
@@ -72,11 +82,22 @@ const BlogDetails = () => {
         {/* Efecto blur de la imagen de fondo del banner */}
         <Box className="blog-detail__backdrop-blur"/>
 
-        <img
-          className="blog-detail__img"
-          src={blogDetails!.imageUrl}
-          alt={blogDetails!.title}
-        />
+        <Box className="blog-detail__img">
+          <img
+            src={blogDetails!.imageUrl}
+            alt={blogDetails!.title}
+          />
+          <Tooltip title="Open image">
+            <IconButton
+              className="blog-detail__img__btn"
+              size="small"
+              onClick={() => setOpenImageModal(true)}
+            >
+              <AiOutlineCamera className="blog-detail__img__btn__icon" />
+            </IconButton>
+          </Tooltip>
+          <Box className="blog-detail__img__overlay" />
+        </Box>
       </Box>
 
       <Box className="blog-detail__content inner-wrapper--sm">
