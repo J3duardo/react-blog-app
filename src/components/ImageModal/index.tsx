5cdf1ddfee504d2a5542e8c4ae982a1b6df875b5
J3duardo <jegq.dev.projects@gmail.com>
2023-01-03 10:47:54 -0400

    Modal genérico para mostrar imágenes

---
+++ src/components/ImageModal/index.tsx
@@ -0,0 +1,46 @@
+import {Dispatch, SetStateAction} from "react";
+import { Box, Dialog, DialogContent, IconButton } from "@mui/material";
+import { useSelector } from "react-redux";
+import { FaTimes } from "react-icons/fa";
+import { LayoutState } from "../../redux/store";
+import "./imageModal.css";
+
+interface Props {
+  image: string;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>
+};
+
+const ImageModal = ({image, open, setOpen}: Props) => {
+  const {navbarHeight} = useSelector((state: LayoutState) => state.layout);
+
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="md"
+      className="image-dialog"
+      open={open}
+      PaperProps={{
+        style: {
+          marginTop: `calc(${navbarHeight}px + var(--spacing-xl))`
+        }
+      }}
+      onClose={() => setOpen(false)}
+    >
+      <DialogContent className="image-dialog__content">
+        <IconButton
+          size="small"
+          className="image-dialog__close-icon"
+          onClick={() => setOpen(false)}
+        >
+          <FaTimes />
+        </IconButton>
+        <Box className="image-dialog__img-wrapper">
+          <img className="image-dialog__img" src={image} alt="" />
+        </Box>
+      </DialogContent>
+    </Dialog>
+  )
+};
+
+export default ImageModal;
