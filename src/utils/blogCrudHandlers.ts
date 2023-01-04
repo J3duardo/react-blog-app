2d69407f0e2e424a967aad7dd93958ef6d637875
J3duardo <jegq.dev.projects@gmail.com>
2023-01-04 17:09:51 -0400

    Utilidad para consultar la data de un blog y actualizar su contador de views

--- src/utils/blogCrudHandlers.ts
+++ src/utils/blogCrudHandlers.ts
@@ -1,16 +1,74 @@
 import { AnyAction, Dispatch as ReduxDispatch } from "@reduxjs/toolkit";
 import { AuthError } from "firebase/auth";
-import { deleteDoc, doc, FirestoreError } from "firebase/firestore";
+import { deleteDoc, doc, DocumentData, DocumentReference, FirestoreError, getDoc, setDoc } from "firebase/firestore";
 import { deleteObject, ref, StorageError } from "firebase/storage";
 import { db, storage } from "../firebase";
-import { generateFirebaseAuthErrorMsg, generateFirebaseStorageErrorMsg, generateFirestoreErrorMsg } from "./firebaseErrorMessages";
 import { setOpen } from "../redux/features/snackbarSlice";
 import { Blog } from "../components/HomePage/BlogSection";
+import { getUserIp } from "./auth";
+import { generateFirebaseAuthErrorMsg, generateFirebaseStorageErrorMsg, generateFirestoreErrorMsg } from "./firebaseErrorMessages";
 
 export interface DeleteBlogConfig {
   blogData: Blog;
   dispatch: ReduxDispatch<AnyAction>;
 };
+
+/**
+ * Consultar la data del blog y actualizar el contador de views.
+ * @param blogRef Referencia del documento de Firebase del blog a consultar.
+ * @param viewsRef Referencia del documento de Firebase de los views del blog a consultar.
+ * @returns Data del blog y número de views.
+ */
+export const getBlogData = async (
+  blogRef: DocumentReference<DocumentData>,
+  viewsRef: DocumentReference<DocumentData>
+) => {
+  // Consultar la data del blog
+  const blogSnapshot = await getDoc(blogRef);
+
+  // Consultar los views del blog
+  const viewsSnapshot = await getDoc(viewsRef);
+
+  // Retornar con error si el blog no existe
+  if(!blogSnapshot.exists()) {
+    throw new Error("Blog not found or deleted")
+  };
+
+  // Extraer la data del blog
+  const blogData = blogSnapshot.data() as Blog;
+
+  // Consultar la IP del usuario
+  const ip = await getUserIp();
+  let currentViewsIps: string[] = [];
+  let currentViewsCount: number = 0;
+
+  // Verificar si ya existe la colección de views del blog
+  if(viewsSnapshot.exists()) {
+    currentViewsIps = viewsSnapshot.data().views as string[];
+    currentViewsCount = currentViewsIps.length;
+  };
+
+  // Si la IP actual no está en los views del blog, agregarla.
+  if(ip && !currentViewsIps.includes(ip)) {
+    currentViewsIps.push(ip);
+
+    await setDoc(
+      viewsRef,
+      {
+        blogId: blogSnapshot.id,
+        views: currentViewsIps,
+        viewsCount: currentViewsIps.length
+      },
+      {merge: true}
+    );
+  };
+
+  return {
+    blogData,
+    currentViewsCount
+  };
+};
+
 
 /**
  * Eliminar un blog
