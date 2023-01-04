c795c1a978f2ae8280aec6dc20ffcc08701a8253
J3duardo <jegq.dev.projects@gmail.com>
2023-01-03 21:05:18 -0400

    Cambio de familia tipográfica

--- src/index.tsx
+++ src/index.tsx
@@ -8,16 +8,17 @@ import "./index.css";
 
 const theme = createTheme({
   typography: {
-    h1: {fontSize: "var(--heading-1)"},
-    h2: {fontSize: "var(--heading-2)"},
-    h3: {fontSize: "var(--heading-3)"},
-    h4: {fontSize: "var(--heading-4)"},
-    h5: {fontSize: "var(--heading-5)"},
-    body1: {fontSize: "var(--paragraph)"},
+    h1: {fontSize: "var(--heading-1)", lineHeight: "var(--line-height-titles)"},
+    h2: {fontSize: "var(--heading-2)", lineHeight: "var(--line-height-titles)"},
+    h3: {fontSize: "var(--heading-3)", lineHeight: "var(--line-height-titles)"},
+    h4: {fontSize: "var(--heading-4)", lineHeight: "var(--line-height-titles)"},
+    h5: {fontSize: "var(--heading-5)", lineHeight: "var(--line-height-titles)"},
+    body1: {fontSize: "var(--paragraph)", lineHeight: "var(--line-height)"},
+    body2: {fontSize: "var(--paragraph)", lineHeight: "var(--line-height)"},
     button: {fontSize: "var(--paragraph)"},
     subtitle1: {fontSize: "var(--text-small)"},
     allVariants: {
-      fontFamily: "'Noto Sans', sans-serif",
+      fontFamily: "'Raleway', sans-serif",
     }
   }
 });
