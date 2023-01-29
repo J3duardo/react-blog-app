88fecb2fcd56fe4ef659505fcc6ba05bc4a0fe39
J3duardo <jegq.dev.projects@gmail.com>
2023-01-29 19:06:40 -0400

    Corrección de excepción generada al formatear la fecha null generada por la funcionalidad en tiempo real para actualizar la lista de comentarios

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -6,6 +6,10 @@ import { Timestamp } from "firebase/firestore";
  * @param date Representación `Timestamp` de la fecha generada por Firebase.
  * @param format String opcional para formatear la fecha.
  */
-export const dateFormatter = (date: Timestamp, format = "MM/DD/YYYY - h:mm a") => {
+export const dateFormatter = (date: Timestamp | null, format = "MM/DD/YYYY - h:mm a") => {
+  if(!date) {
+    return dayjs(Date.now()).format(format)
+  };
+
   return dayjs(date.toDate()).format(format);
 };
