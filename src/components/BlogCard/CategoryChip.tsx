3281499c98f6f2f4ce2886d925228879aea2358f
Jesús Guzmán <jegq.dev.projects@gmail.com>
2023-02-11 22:13:32 -0400

    Navegar a la página de búsquedas al clickear el item de la categoría

--- src/components/BlogCard/CategoryChip.tsx
+++ src/components/BlogCard/CategoryChip.tsx
@@ -1,4 +1,5 @@
 import { Chip } from "@mui/material";
+import { Link } from "react-router-dom";
 
 interface Props {
   category: string;
@@ -6,12 +7,19 @@ interface Props {
 
 const CategoryChip = ({category}: Props) => {
   return (
-    <Chip
-      variant="filled"
-      label={category}
-      size="small"
-      color="primary"
-    />
+    <Link
+      style={{textDecoration: "none"}}
+      to={`/blog/search?category=${category}`}
+    >
+      <Chip
+        style={{cursor: "pointer"}}
+        variant="filled"
+        label={category}
+        size="small"
+        color="primary"
+        component="span"
+      />
+    </Link>
   )
 };
 
