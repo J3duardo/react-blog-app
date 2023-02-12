5630d429b50bdc19750ba7daec38d0fb1a3ac180
Jesús Guzmán <jegq.dev.projects@gmail.com>
2023-02-11 22:16:49 -0400

    Permitir cualquier caracter en el título del post

--- src/components/CreateBlogFormElements/BlogTitleField.tsx
+++ src/components/CreateBlogFormElements/BlogTitleField.tsx
@@ -2,7 +2,7 @@ import { TextField } from "@mui/material";
 import { useFormContext } from "react-hook-form";
 import ValidationErrorMsg from "../AuthFormsElements/ValidationErrorMsg";
 
-const TITLE_REGEX = /^[A-Za-zÀ-ž0-9()\-\s]{6,50}$/;
+// const TITLE_REGEX = /^[A-Za-zÀ-ž0-9()¡!\-\?¿\s]{6,50}$/;
 
 interface Props {
   disabled: boolean;
@@ -28,10 +28,10 @@ export const BlogTitleField = ({disabled}: Props) => {
         required: {value: true, message: "The title is required"},
         minLength: {value: 6, message: "The title must contain at least 6 characters"},
         maxLength: {value: 50, message: "The title must contain maximum 50 characters"},
-        pattern: {
-          value: TITLE_REGEX,
-          message: "The title must contain only alphanumeric characters, hyphens and parentheses"
-        }
+        // pattern: {
+        //   value: TITLE_REGEX,
+        //   message: "The title must contain only letters, numbers, parentheses, exclamation, interrogation and hyphens"
+        // }
       })}
     />
   )
