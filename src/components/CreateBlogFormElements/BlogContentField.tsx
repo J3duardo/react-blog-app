40cc5d075a2366484d25b65ca2f729318931243e
Jesús Guzmán <jegq.dev.projects@gmail.com>
2023-02-15 18:14:23 -0400

    No permitir heading H1 en el contenido del post (Sólo el título debe ser H1)

--- src/components/CreateBlogFormElements/BlogContentField.tsx
+++ src/components/CreateBlogFormElements/BlogContentField.tsx
@@ -10,7 +10,7 @@ const modules = {
   toolbar: [
     ["bold", "italic", "underline", "strike"],
     [{ list: "ordered"}, { list: "bullet" }],
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ header: [2, 3, 4, 5, 6, false] }],
     ["clean"]
   ],
 };
