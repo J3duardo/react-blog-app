31c473dce47d37d725fcaa631117860ef736a857
Jesús Guzmán <jegq.dev.projects@gmail.com>
2023-02-23 23:35:46 -0400

    Menú de navegación mobile

---
+++ src/components/NavBar/MobileDrawerItem.tsx
@@ -0,0 +1,54 @@
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, Divider, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { IconType } from "react-icons/lib";
+
+interface Props {
+  Icon: IconType;
+  text: string;
+  path: string;
+  disabled?: boolean;
+  onClick?: () => void;
+};
+
+const MobileDrawerItem = ({Icon, text, path, disabled, onClick}: Props) => {
+  return (
+    <>
+      <ListItem disablePadding>
+        <NavLink
+          style={{width: "100%", color: "inherit", textDecoration: "none"}}
+          to={path}
+          onClick={(e) => path === "#" && e.preventDefault()}
+        >
+          {({isActive}) => {
+            return (
+              <ListItemButton
+                style={{
+                  backgroundColor: (isActive && path !== "#") ? "rgba(0,0,0,0.1)" : "transparent"
+                }}
+                disabled={disabled}
+                onClick={() => onClick?.()}
+              >
+                <ListItemIcon>
+                  <Icon style={{width: "var(--heading-4)", height: "var(--heading-4)"}} />
+                </ListItemIcon>
+                <ListItemText
+                  primary={
+                    <Typography
+                      style={{fontWeight: isActive ? 700 : 400}}
+                      variant="body1"
+                    >
+                      {text}
+                    </Typography>
+                  }
+                />
+              </ListItemButton>
+            )
+          }}
+        </NavLink>
+      </ListItem>
+      <Divider />
+    </>
+  )
+};
+
+export default MobileDrawerItem;
---
+++ src/components/NavBar/NavbarMobileDrawer.tsx
@@ -0,0 +1,112 @@
+import {Dispatch, SetStateAction} from "react";
+import {Drawer, Box, List, ListItem, ListItemButton, Avatar, Typography, ListItemIcon, ListItemText, Divider} from "@mui/material";
+import {NavLink} from "react-router-dom";
+import {AiOutlineHome, AiOutlineLogin, AiOutlineUser} from "react-icons/ai";
+import {BsPencilSquare} from "react-icons/bs";
+import {RiLogoutCircleLine} from "react-icons/ri";
+import MobileDrawerItem from "./MobileDrawerItem";
+import {UserData} from "../../redux/features/authSlice";
+
+const MENU_ITEMS_NOAUTH = [
+  {id: 1, title: "Login", to: "/login", Icon: AiOutlineLogin},
+  {id: 2, title: "Signup", to: "/signup", Icon: AiOutlineUser}
+];
+
+interface Props {
+  user: UserData | null;
+  open: boolean;
+  navbarHeight: number;
+  isLoggingOut: boolean;
+  logoutHandler: () => Promise<void>
+  setOpen: Dispatch<SetStateAction<boolean>>
+};
+
+const NavbarMobileDrawer = ({user, open, navbarHeight, isLoggingOut, logoutHandler, setOpen}: Props) => {
+  return (
+    <Drawer
+      className="navbar__items__drawer"
+      anchor="left"
+      open={open}
+      onClose={() => setOpen(false)}
+    >
+      <Box
+        style={{paddingTop: `${navbarHeight}px`}}
+        className="navbar__mobile-drawer__content"
+        component="nav"
+      >
+        <List>
+          <>
+            <MobileDrawerItem
+              Icon={AiOutlineHome}
+              text="My Blog"
+              path="/"
+            />
+
+            {!user &&
+              MENU_ITEMS_NOAUTH.map((item) => (
+                <MobileDrawerItem
+                  key={item.id}
+                  Icon={item.Icon}
+                  text={item.title}
+                  path={item.to}
+                />
+              ))
+            }
+
+            {user &&
+              <>
+                <ListItem disablePadding>
+                  <NavLink
+                    style={{width: "100%", color: "inherit", textDecoration: "none"}}
+                    to={`/profile`}
+                  >
+                    {({isActive}) => (
+                      <ListItemButton
+                        style={{backgroundColor: isActive ? "rgba(0,0,0,0.1)" : "transparent"}}
+                      >
+                        <ListItemIcon>
+                          <Avatar
+                            style={{width: "var(--heading-4)", height: "var(--heading-4)"}}
+                            src={user.photoURL || ""}
+                            alt={user.displayName}
+                          />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={
+                            <Typography
+                              style={{fontWeight: isActive ? 700 : 400}}
+                              variant="body1"
+                            >
+                              {user.displayName.split(" ")[0]}
+                            </Typography>
+                          }
+                        />
+                      </ListItemButton>
+                    )}
+                  </NavLink>
+                </ListItem>
+
+                <Divider />
+
+                <MobileDrawerItem
+                  Icon={BsPencilSquare}
+                  text="Create"
+                  path="/blog/create"
+                />
+                
+                <MobileDrawerItem
+                  Icon={RiLogoutCircleLine}
+                  text="Logout"
+                  path="#"
+                  onClick={() => logoutHandler()}
+                />
+              </>
+            }
+          </>
+        </List>
+      </Box>
+    </Drawer>
+  )
+};
+
+export default NavbarMobileDrawer;
